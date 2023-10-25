05a75661e506b6a658c28ead4e57bba438181987
wojzam <wojciech.zamarski@student.pk.edu.pl>
2023-10-25 14:58:34 +0200

    redesign analysis module

--- analysis.py
+++ analysis.py
@@ -1,5 +1,7 @@
+import os
+
 import matplotlib.pyplot as plt
-from tabulate import tabulate
+import pandas as pd
 
 from genetic_algorithm.strategies import *
 from model import Model
@@ -12,16 +14,17 @@ class Analysis:
         np.random.seed(0)
         self.model = Model()
 
-    def run(self, generation_range, crossover_methods, iterations=10):
+    def run(self, generations, crossover_methods, iterations=10):
         self.generate_problem()
 
         for crossover_method in crossover_methods:
-            results = self.get_results(iterations, generations=max(generation_range), crossover_method=crossover_method)
+            results = self.get_results(iterations, generations=generations, crossover_method=crossover_method)
             best_scores, mean_scores, std_scores = zip(
-                *(self.calculate_scores_statistics(results, gen) for gen in generation_range))
+                *(self.calculate_scores_statistics(results, gen) for gen in range(generations)))
 
-            self.plot_results(generation_range, best_scores, mean_scores, std_scores, self.get_name(crossover_method))
-            self.print_table(generation_range, best_scores, mean_scores, std_scores, self.get_name(crossover_method))
+            args = range(1, generations + 1), best_scores, mean_scores, std_scores, crossover_method
+            self.plot_results(*args)
+            self.save_to_file(*args)
 
     def generate_problem(self, customer_count=DEFAULT_CUSTOMER_COUNT):
         self.model.generate_customers(customer_count)
@@ -36,40 +39,37 @@ class Analysis:
         return results
 
     @staticmethod
-    def get_name(crossover_method):
-        return crossover_method.__name__.replace("_", " ")
-
-    @staticmethod
-    def calculate_scores_statistics(results, generation=0):
-        scores = [result.best_scores_history[generation - 1] for result in results]
+    def calculate_scores_statistics(results, generation=-1):
+        scores = [result.best_scores_history[generation] for result in results]
         return np.min(scores), np.mean(scores), np.std(scores)
 
     @staticmethod
-    def plot_results(generations_range, best_scores, mean_scores, std_scores, method_name):
-        x_values = range(len(generations_range))
+    def plot_results(generations, best_scores, mean_scores, std_scores, method):
         plt.figure(figsize=(10, 5))
-        plt.bar(x_values, best_scores, label='Best Score')
-        plt.errorbar(x_values, mean_scores, yerr=std_scores, fmt='o', label='Std Score')
+        plt.plot(generations, best_scores, label='Best Score')
+        plt.plot(generations, mean_scores, label='Mean Score')
+        n = 10  # Display every nth point
+        plt.errorbar(generations[::n], mean_scores[::n], yerr=std_scores[::n], fmt='o', label='Std Score', color="C3")
+        plt.errorbar(generations[-1], mean_scores[-1], yerr=std_scores[-1], fmt='o', color="C3")
+        plt.xlabel('Generation')
         plt.ylabel('Score')
-        plt.title(f'Scores vs. Generations ({method_name})')
-        plt.xticks(x_values, [f'{gen} Gen' for gen in generations_range])
+        plt.title(f'Scores vs. Generation ({method.__name__.replace("_", " ")})')
         plt.legend()
         plt.grid(True)
         plt.show()
 
     @staticmethod
-    def print_table(generation_range, best_scores, mean_scores, std_scores, method_name):
-        data = {
-            "Generation": generation_range,
+    def save_to_file(generations, best_scores, mean_scores, std_scores, method, directory="analysis_results"):
+        os.makedirs(directory, exist_ok=True)
+        df = pd.DataFrame({
+            "Generation": generations,
             "Best Score": np.round(best_scores, 2),
             "Mean Score": np.round(mean_scores, 2),
             "Std. Score": np.round(std_scores, 2),
-        }
-        print(f"\n=== Results for {method_name} ===")
-        print(tabulate(data, headers="keys"))
+        })
+        df.to_csv(os.path.join(directory, f"{method.__name__}.csv"), index=False)
 
 
 if __name__ == "__main__":
     analysis = Analysis()
-    analysis.run([10, 50, 100, 200, 500],
-                 [order_crossover, order_based_crossover, partially_mapped_crossover, cycle_crossover])
+    analysis.run(800, [order_crossover, order_based_crossover, partially_mapped_crossover, cycle_crossover])
