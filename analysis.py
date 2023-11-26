9de228881ea31cdf20777fac625e4112616c3f1c
wojzam <wojciech.zamarski@student.pk.edu.pl>
2023-11-26 11:58:35 +0100

    rework analysis module

--- analysis.py
+++ analysis.py
@@ -1,7 +1,6 @@
 import os
 
 import matplotlib.pyplot as plt
-import optuna
 import pandas as pd
 
 from genetic_algorithm.strategies import *
@@ -9,56 +8,64 @@ from model import Model
 
 
 class Analysis:
-    DEFAULT_CUSTOMER_COUNT = 15
-    DEFAULT_ITERATIONS = 10
+    GENERATIONS = 100
+    TEST_ITERATIONS = 10
     RESULTS_DIRECTORY = "analysis_results"
-    ALL_CROSSOVER_METHODS = [order_crossover, order_based_crossover, partially_mapped_crossover, cycle_crossover,
-                             edge_recombination_crossover]
+    ALL_CROSSOVER_METHODS = [order_crossover, order_based_crossover, position_based_crossover,
+                             partially_mapped_crossover, cycle_crossover, edge_recombination_crossover]
 
-    def __init__(self, seed=0):
-        np.random.seed(seed)
+    def __init__(self, customer_count=15):
         self.model = Model()
+        self.customer_count = customer_count
 
-    def run(self, generations, crossover_methods, iterations=DEFAULT_ITERATIONS):
+    def analyse_crossovers(self, crossover_methods, iterations=TEST_ITERATIONS, generations=GENERATIONS, **kwargs):
         self._generate_problem()
         method_scores = []
 
         for method in crossover_methods:
-            results = self._get_results(iterations, generations=generations, crossover_method=method)
+            results = self._get_results(iterations, generations=generations, crossover_method=method, **kwargs)
             best_scores, mean_scores, std_scores = zip(
-                *(self._calculate_scores_statistics(results, gen) for gen in range(generations)))
+                *(self._calculate_scores_statistics(results, gen) for gen in range(generations + 1)))
 
             method_scores.append((pretty_name(method), mean_scores))
 
-            self._plot_scores(range(1, generations + 1), best_scores, mean_scores, std_scores, pretty_name(method))
-            self._save_to_file(range(1, generations + 1), best_scores, mean_scores, std_scores, method.__name__)
+            self._plot_scores(range(generations + 1), best_scores, mean_scores, std_scores, pretty_name(method))
+            self._save_to_file({
+                "Generation": range(generations + 1),
+                "Best Score": best_scores,
+                "Mean Score": mean_scores,
+                "Std. Score": std_scores},
+                method.__name__)
 
         self._plot_method_comparison(method_scores)
 
-    def optimize_hyperparameters(self, generations, crossover_method, iterations=DEFAULT_ITERATIONS, n_trials=100):
+    def analyse_pc_impact(self, crossover_methods, iterations=TEST_ITERATIONS, generations=GENERATIONS, **kwargs):
         self._generate_problem()
+        probabilities = np.arange(0.0, 1.0, 0.1)
+        method_final_scores = []
 
-        def objective(trial):
-            pc = trial.suggest_float('pc', 0.3, 0.8)
-            pm = trial.suggest_float('pm', 0.01, 0.3)
-            results = self._get_results(iterations, generations=generations, pc=pc, pm=pm,
-                                        crossover_method=crossover_method)
+        for method in crossover_methods:
+            mean_final_scores = []
 
-            best_score, mean_score, std_score = self._calculate_scores_statistics(results)
-            return best_score + mean_score
+            for pc in probabilities:
+                results = self._get_results(iterations, generations=generations, crossover_method=method, pc=pc,
+                                            **kwargs)
+                best_score, mean_score, std_score = self._calculate_scores_statistics(results)
+                mean_final_scores.append(mean_score)
 
-        study = optuna.create_study()
-        study.optimize(objective, n_trials=n_trials)
+            method_final_scores.append((pretty_name(method), mean_final_scores))
+            self._save_to_file({"PC": probabilities, "Mean score": mean_final_scores}, method.__name__)
 
-        print(study.best_params)
+        self._plot_pc_method_comparison(probabilities, method_final_scores)
 
-    def _generate_problem(self, customer_count=DEFAULT_CUSTOMER_COUNT):
-        self.model.generate_customers(customer_count)
+    def _generate_problem(self):
+        self.model.generate_customers(self.customer_count)
 
     def _get_results(self, iterations, output=False, **kwargs):
         results = []
 
-        for _ in range(iterations):
+        for i in range(iterations):
+            np.random.seed(i)
             self.model.generate_routes(output=output, show_plot=output, **kwargs)
             results.append(self.model.result)
 
@@ -70,7 +77,7 @@ class Analysis:
         return np.min(scores), np.mean(scores), np.std(scores)
 
     @staticmethod
-    def measure_crossover_impact(population_size=100, iterations=1000):
+    def analyse_crossover_impact_on_offsprings(population_size=100, iterations=1000):
         def impact(arr1, arr2):
             return 1.0 - np.mean(arr1 == arr2)
 
@@ -105,6 +112,18 @@ class Analysis:
             Analysis._plot_method_comparison(method_scores)
 
     @staticmethod
+    def plot_pc_method_comparison_from_files(files):
+        method_final_scores = []
+        probabilities = []
+
+        for file in files:
+            probabilities, mean_scores = analysis._read_file(file)
+            method_final_scores.append((file, mean_scores))
+
+        if method_final_scores:
+            Analysis._plot_pc_method_comparison(probabilities, method_final_scores)
+
+    @staticmethod
     def _plot_scores(generations, best_scores, mean_scores, std_scores, method_name):
         plt.figure(figsize=(10, 5))
         plt.plot(generations, best_scores, label='Best Score')
@@ -120,9 +139,9 @@ class Analysis:
         plt.show()
 
     @staticmethod
-    def _plot_method_comparison(scores_methods):
+    def _plot_method_comparison(method_scores):
         plt.figure(figsize=(10, 5))
-        for method_name, scores in scores_methods:
+        for method_name, scores in method_scores:
             plt.plot(range(1, len(scores) + 1), scores, label=method_name)
         plt.xlabel('Generation')
         plt.ylabel('Mean Score')
@@ -132,15 +151,21 @@ class Analysis:
         plt.show()
 
     @staticmethod
-    def _save_to_file(generations, best_scores, mean_scores, std_scores, file_name, directory=RESULTS_DIRECTORY):
+    def _plot_pc_method_comparison(probabilities, method_final_scores):
+        plt.figure(figsize=(10, 5))
+        for method_name, scores in method_final_scores:
+            plt.plot(probabilities, scores, label=method_name)
+        plt.xlabel('Crossover probability')
+        plt.ylabel('Mean score')
+        plt.title(f'Scores vs. Pc')
+        plt.legend()
+        plt.grid(True)
+        plt.show()
+
+    @staticmethod
+    def _save_to_file(dictionary, file_name, directory=RESULTS_DIRECTORY):
         os.makedirs(directory, exist_ok=True)
-        df = pd.DataFrame({
-            "Generation": generations,
-            "Best Score": np.round(best_scores, 2),
-            "Mean Score": np.round(mean_scores, 2),
-            "Std. Score": np.round(std_scores, 2),
-        })
-        df.to_csv(os.path.join(directory, f"{file_name}.csv"), index=False)
+        pd.DataFrame(dictionary).round(2).to_csv(os.path.join(directory, f"{file_name}.csv"), index=False)
 
     @staticmethod
     def _read_file(file_name, directory=RESULTS_DIRECTORY):
@@ -159,12 +184,18 @@ def pretty_name(crossover_method):
 
 if __name__ == "__main__":
     analysis = Analysis()
+    analyzed_crossover_methods = [order_crossover, order_based_crossover, partially_mapped_crossover, cycle_crossover]
+    file_names = [method.__name__ for method in analyzed_crossover_methods]
 
     # Example 1
-    analysis.run(800, Analysis.ALL_CROSSOVER_METHODS)
+    # analysis.analyse_crossovers(analyzed_crossover_methods, generations=50)
+    # analysis.plot_method_comparison_from_files(file_names)
 
     # Example 2
-    # analysis.optimize_hyperparameters(500, order_crossover)
+    # analysis.analyse_pc_impact(analyzed_crossover_methods, generations=500)
+    # analysis.plot_pc_method_comparison_from_files(file_names)
 
     # Example 3
-    analysis.measure_crossover_impact()
+    # analysis.analyse_crossover_impact_on_offsprings()
+
+
